3498dba58f42124814cfee7f48a49b03c7fb96ad
jim0409 <jim.weng@prophetstor.com>
2017-11-20 14:49:50 +0800

    Add testcode in string folder

--- GO_importPackage/header.go
+++ GO_importPackage/header.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/goPractice/GO_importPackage/goexample"
+	"github.com/goPractice/GO_importPackage/string"
 )
 
-func main(){
+func main() {
 	goexample.Hello()
 	goexample.Hi()
+	var test = string.Reverse("Hello")
+	fmt.Println(test)
 }
