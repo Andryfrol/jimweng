3bdae235015786dc646098b6b91557e57d84d32e
jim0409 <jim.weng@prophetstor.com>
2017-11-21 19:57:07 +0800

    Error handling

--- ATourOfGO/goErrorhandling.go
+++ ATourOfGO/goErrorhandling.go
@@ -1,17 +1,47 @@
 // https://blog.golang.org/error-handling-and-go
+// https://gobyexample.com/errors
 package main
 
 import (
-	"log"
-	"os"
+	"errors"
+	"fmt"
 )
 
-func Open(name string) (file *File, err error)
+func f1(arg int) (int, error) {
+	if arg == 42 {
+		return -1, errors.New("can't work with 42")
+	}
+	return arg + 3, nil
+}
+
+type argError struct {
+	arg  int
+	prob string
+}
+
+func (e *argError) Error() string {
+	return fmt.Sprintf("%d - %s", e.arg, e.prob)
+}
+
+func f2(arg int) (int, error) {
+	if arg == 42 {
+		return -1, &argError{arg, "can't work with it"}
+	}
+	return arg + 3, nil
+}
 
 func main() {
-	f, err := os.Open("filename.ext")
-	if err != nil {
-		log.Fatal(err)
+	for _, i := range []int{7, 42} {
+		if r, e := f1(i); e != nil {
+			fmt.Println("f1 failed:", e)
+		} else {
+			fmt.Println("f2 worked:", r)
+		}
 	}
 
+	_, e := f2(42)
+	if ae, ok := e.(*argError); ok {
+		fmt.Println(ae.arg)
+		fmt.Println(ae.prob)
+	}
 }
