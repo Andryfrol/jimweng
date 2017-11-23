84f3871f68a3cf69f44e2c5c37bf45b91b4f66d1
jim0409 <jim.weng@prophetstor.com>
2017-11-23 11:17:58 +0800

    commit mysecondgo

---
+++ GoStructPractice/mysecondgo/plugins/all/all.go
@@ -0,0 +1,8 @@
+package all
+
+import (
+	_ "github.com/influxdata/telegraf/plugins/mysql"
+	_ "github.com/influxdata/telegraf/plugins/postgresql"
+	_ "github.com/influxdata/telegraf/plugins/redis"
+	_ "github.com/influxdata/telegraf/plugins/system"
+)
