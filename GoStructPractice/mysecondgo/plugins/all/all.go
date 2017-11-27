af81cf9bafd0e587f0772852afb23cbdf20bbcc0
jim0409 <jim.weng@prophetstor.com>
2017-11-27 09:38:25 +0800

    delete mysecond proj

--- GoStructPractice/mysecondgo/plugins/all/all.go
+++
@@ -1,8 +0,0 @@
-package all
-
-import (
-	_ "github.com/influxdata/telegraf/plugins/mysql"
-	_ "github.com/influxdata/telegraf/plugins/postgresql"
-	_ "github.com/influxdata/telegraf/plugins/redis"
-	_ "github.com/influxdata/telegraf/plugins/system"
-)
