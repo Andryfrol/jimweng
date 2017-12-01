7d984289ee6db8bef2f5f6648e8beb1e6be90301
jim0409 <jim.weng@prophetstor.com>
2017-12-01 15:03:26 +0800

    add go test

--- ATourOfGO/basic/gotest.go
+++ ATourOfGO/basic/gotest.go
@@ -6,9 +6,18 @@ import (
 )
 
 var fDebug = flag.Bool("debug", true, "show debug mode help")
+var fString = flag.String("name","Jim","default name would be Jim")
 
 type test struct {
 	Debug bool
+}
+func StringInSlice(a string, list []string) bool{
+	for _, b := range list{
+		if b == a{
+			return true
+		}
+	}
+	return false
 }
 
 // go run gotest.go -debug=false
@@ -18,4 +27,12 @@ func main() {
 	if *fDebug {
 		fmt.Println("it's right")
 	}
+
+	fmt.Println(*fString)
+
+	list :=[]string{"jim","bob","beck"}
+	if StringInSlice(*fString, list) {
+		fmt.Println("true")
+	}
+
 }
