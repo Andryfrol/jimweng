554022369a9c037eb926f61216d8b215007ab584
jim0409 <jim.weng@prophetstor.com>
2017-12-20 15:14:52 +0800

    update

--- ATourOfGO/basic/operation/goUnmarshal.go
+++ ATourOfGO/basic/operation/goUnmarshal.go
@@ -26,12 +26,19 @@ func main() {
 		}`
 	res := Response{}
 	json.Unmarshal([]byte(str), &res)
+	fmt.Println(res)
 	fmt.Println(res.Fruits[0].([]interface{})[0])
 	test :=res.Fruits[0].([]interface{})[0]
 	fmt.Println(reflect.TypeOf(test))
 	fmt.Println(test.(string))
 
+	strs :=`{"name" :"data"}`
+	var f map[string]interface{}
 
+	json.Unmarshal([]byte(strs), &f)
+	fmt.Println(f)
+	// fmt.Println(testf["name"])
+	// fmt.Println(f.(map[string]interface{})["name"])
 	// strs := `{
 	// 	"columns" : [ "id(r)", "n1.domainId", "type(r)", "n2.domainId" ],
 	// 	"data" : [ [ 74, "testDomainID3", "take", "testDomainID" ], [ 73, "testDomainID", "belong", "testDomainID2" ], [ 75, "testDomainID2", "follow", "testDomainID3" ], [ 76, "testDomainID", "after", "testDomainID3" ] ]
