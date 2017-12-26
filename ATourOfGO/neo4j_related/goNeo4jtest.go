a22f84aca2c7006befa57301354dc53293078cd7
jim0409 <jim.weng@prophetstor.com>
2017-12-26 23:12:18 +0800

    show nodes

--- ATourOfGO/neo4j_related/goNeo4jtest.go
+++ ATourOfGO/neo4j_related/goNeo4jtest.go
@@ -1,34 +1,91 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"io/ioutil"
-	"net/http"
-	"strings"
 	"fmt"
 )
 
-// var testString = "neo4j,nodeNum=n1,tag=testTag,domainID=testDomainID, name=testName,link=n1_n2|n3_n1,relation=belong|take,nodeNum=n2,tag=testTag2,domainID=testDomainID2, name=testName2,link=n2_n3,relation=follow,nodeNum=n3,tag=testTag3,domainID=testDomainID3, name=testName3,link=n1_n3,relation=after"
-
 // claim node : would declare properties that would be writed to neo4j
-type neo4jMetric struct {
-	NAME string `neo4j`
-	NodeNum map[string]nodeInfo{}
+type nodeInfo struct {
+	NodeNum  string
+	Type     string
+	DomainID string
+	Name     string
+	Labels   string
+	// Link     string
+	// Relation string
 }
 
-type nodeInfo struct {
-	TAG      string `tag`
-	DomainID string `domainID`
-	NAME	string `name`
-	Link     string	`link`
-	Relation string `relation`
+// declare to parse json format while use NewRequest
+type Payload struct {
+	Query string `json:"query"`
+}
+
+func neo4jBodyString(c nodeInfo) string {
+	var bodyString = "merge (" + c.NodeNum + ":" + c.Labels + " {domainId:'" + c.DomainID + "', name:'" + c.Name + "'})"
+	return bodyString
+}
+
+// func neo4jLinkString(c nodeInfo) string {
+// 	var linkString string
+// 	if len(strings.Split(c.Link, "|")) == 1 {
+// 		linkString = "merge " + c.Link + " = (" + strings.Split(c.Link, "_")[0] + ")-[:" + c.Relation + "]->(" + strings.Split(c.Link, "_")[1] + ")"
+// 	} else {
+// 		linkArray := strings.Split(c.Link, "|")
+// 		linkRelation := strings.Split(c.Relation, "|")
+// 		for i := 0; i < len(linkArray); i++ {
+// 			tempString := "merge " + linkArray[i] + " = (" + strings.Split(linkArray[i], "_")[0] + ")-[:" + linkRelation[i] + "]->(" + strings.Split(linkArray[i], "_")[1] + ")"
+// 			linkString = linkString + " " + tempString
+// 		}
+// 	}
+// 	return linkString
+// }
+
+func neo4jMainFunc(m map[string]nodeInfo) string {
+	// define parse body
+	var parseBodyString string
+	var parselinkString string
+	for value := range m {
+
+		nodesCreated := nodeInfo{
+			NodeNum:  m[value].NodeNum,
+			Type:     m[value].Type,
+			DomainID: m[value].DomainID,
+			Name:     m[value].Name,
+			Labels:   m[value].Labels,
+		}
+
+		fmt.Println(nodesCreated)
+		// 	// if !(nodesCreated.Link == "") {
+		// 	// 	parselinkString = parselinkString + " " + neo4jLinkString(nodesCreated)
+		// 	// }
+		// 	// parseBodyString = parseBodyString + " " + neo4jBodyString(nodesCreated)
+	}
+	parseBodyString = parseBodyString + parselinkString
+	return parseBodyString
 }
 
 func main() {
+	// var onNodeString string
 
-	var test :=neo4jMetric{
-		
+	var neo4jInput = map[string]nodeInfo{
+		// "n1": {"n1", "testDomainID", "testName", "testTag", "n1_n2", "belong"},
+		// "n2": {"n2", "testDomainID2", "testName2", "testTag2", "n2_n3", "follow"},
+		// "n3": {"n3", "testDomainID3", "testName3", "testTag3", "n3_n1", "take"},
+		"n1": {"n1", "node", "testDomainID", "testName", "testTag"},
+		"n2": {"n2", "node", "testDomainID2", "testName2", "testTag2"},
+		"n3": {"n3", "node", "testDomainID3", "testName3", "testTag3"},
+		// "r1": {"n1_n2", "belong"},
+		// "r2": {"n2_n3", "follow"},
+		// "r3": {"n3_n1", "take"},
 	}
 
+	fmt.Println("neo4jInput is", neo4jInput)
+	fmt.Println("----------------------------------------------------------------")
+
+	neo4jMainFunc(neo4jInput)
+
+	// data := Payload{
+	// 	Query: onNodeString,
+	// }
+	// fmt.Println(data)
 }
