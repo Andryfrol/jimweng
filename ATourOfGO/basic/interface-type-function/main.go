f5bc9e9afd1f62d1ac87b7b17b57d0026d4a455e
jim0409 <berserker.01.tw@hotmail.com>
2018-12-20 15:35:39 +0800

    add type STRUCTURE FUN(INPUT_PAR) RETURN_PAR"

---
+++ ATourOfGO/basic/interface-type-function/main.go
@@ -0,0 +1,37 @@
+package main
+
+// refer https://www.jianshu.com/p/fc4902159cf5
+
+// type `Structure` func(`input parameters`) `return parameters`
+// is like decorator method in python
+
+import "fmt"
+
+// Greeting function types
+// 定義Greeting這個資料結構，key是函數(涵括傳入的參數)/value是該函數的返回值
+type Greeting func(name string) string
+
+func say(g Greeting, n string) {
+	fmt.Println("say is processing ... ")
+	fmt.Println(g(n))
+}
+
+func english(name string) string {
+	fmt.Println("english is processing ... ")
+	return "Hello, " + name
+}
+
+func main() {
+
+	say(english, "World")
+
+	// equivalent to below ...
+	fmt.Printf("\n\nis equivalient to below ... \n\n")
+
+	g := Greeting(english)
+	g.say2("World")
+}
+
+func (g Greeting) say2(n string) {
+	fmt.Println(g(n))
+}
