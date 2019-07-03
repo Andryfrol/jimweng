33ded340db4b94239d458900316d66fa30474e29
jim0409 <berserker.01.tw@hotmail.com>
2019-07-03 11:19:31 +0800

    add td search

--- goCrawler/pkgmanagement/plugins/inputs/crawler/crawler.go
+++ goCrawler/pkgmanagement/plugins/inputs/crawler/crawler.go
@@ -10,48 +10,39 @@ type PkgNode struct {
 	Name     string
 	Synopsis string
 	Href     string
-	Child    *PkgNode
+	// Child    *PkgNode
 }
 
 func main() {
-	// var headings, row, row_href []string
-	var row, row_href []string
-	var rows, row_hrefs [][]string
 	doc, err := goquery.NewDocument("https://golang.google.cn/pkg/")
 	if err != nil {
 		fmt.Printf("%v\n", err)
 	}
 
-	// var PkgList []*PkgNode
-	// var pkgnode *PkgNode
+	var PkgList []*PkgNode
 
 	// 尋找table，在這個case只有pkg table
 	doc.Find("table").Each(func(index int, tablehtml *goquery.Selection) {
 		// table 下每一個row即為一個pkg管理套件
 		tablehtml.Find("tr").Each(func(indextr int, rowhtml *goquery.Selection) {
-			// rowhtml.Find("th").Each(func(indexth int, tableheading *goquery.Selection) {
-			// headings = append(headings, tableheading.Text())
-			// })
 			rowhtml.Find("td").Each(func(indextd int, tablecell *goquery.Selection) {
-				// pkgnode.Name = tablecell.Text()
-				row = append(row, tablecell.Text())
+				pkgnode := PkgNode{}
+				pkgnode.Name = tablecell.Text()
+				// row = append(row, tablecell.Text())
 				tablecell.Find("a").Each(func(indexa int, tdcell *goquery.Selection) {
 					href_text, ok := tdcell.Attr("href")
 					if ok {
-						row_href = append(row_href, href_text)
+						pkgnode.Href = href_text
+						// row_href = append(row_href, href_text)
 					}
 				})
-
+				PkgList = append(PkgList, &pkgnode)
 			})
 
-			rows = append(rows, row)
-			row = nil
-
-			row_hrefs = append(row_hrefs, row_href)
-			row_href = nil
 		})
 	})
-	// fmt.Println("####### headings = ", len(headings), headings)
-	fmt.Println("####### rows = ", len(rows), rows)
-	fmt.Println("####### href = ", len(row_hrefs), row_hrefs)
+
+	for i, j := range PkgList {
+		fmt.Printf("%d___%v___%v__%v\n", i, j.Href, j.Name, j.Synopsis)
+	}
 }
