0a10a12d5a896e5cddcac961eb3c2e68453c9878
jim0409 <berserker.01.tw@hotmail.com>
2019-07-23 17:07:28 +0800

    add encode decode method for key file

---
+++ ATourOfGO/crypto/rsa_and_x509/EN_DE_code_with_key/decrypt.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"fmt"
+	"io/ioutil"
+)
+
+func RsaS1Decrypt(baseEncryMsg string) ([]byte, error) {
+	var rsaSertKey, _ = ioutil.ReadFile("./rsa_prv.key")
+	block, _ := pem.Decode(rsaSertKey)
+	if block == nil {
+		return nil, fmt.Errorf("private key error!")
+	}
+
+	// Parse With rsa_privatekey with x509 ParsePKCS1PrivateKey
+	priv, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		tmpStr := fmt.Sprintf("s1 private key err: %v", err)
+		return nil, fmt.Errorf(tmpStr)
+	}
+
+	// base64 decode
+	decryptMsg, err := base64.StdEncoding.DecodeString(baseEncryMsg)
+	if err != nil {
+		tmpStr := fmt.Sprintf("base64 decode err: %v", err)
+		fmt.Println(tmpStr)
+		return nil, fmt.Errorf(tmpStr)
+	}
+
+	return rsa.DecryptPKCS1v15(rand.Reader, priv, decryptMsg)
+}
+
+func main() {
+	ciphertext, err := ioutil.ReadFile("myfile.data")
+	if err != nil {
+		fmt.Printf("error happend while read file : %s", err)
+	}
+	deCodeCtx, err := RsaS1Decrypt(string(ciphertext))
+	if err != nil {
+		fmt.Printf("error happened while decode with err : %s", err)
+	}
+	fmt.Println(string(deCodeCtx))
+}
