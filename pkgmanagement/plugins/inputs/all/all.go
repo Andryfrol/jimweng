67f2612ec7621f0c988dc31f5550ce955424ccf8
jim0409 <berserker.01.tw@hotmail.com>
2019-08-06 17:49:07 +0800

    remove non-necessary folder

--- pkgmanagement/plugins/inputs/all/all.go
+++
@@ -1,5 +0,0 @@
-package all
-
-import (
-	_ "github.com/jimweng/pkgmanagement/plugins/inputs/crawler"
-)
--- pkgmanagement/plugins/outputs/all/all.go
+++
@@ -1,5 +0,0 @@
-package all
-
-import (
-	_ "github.com/jimweng/pkgmanagement/plugins/outputs/mysql"
-)
